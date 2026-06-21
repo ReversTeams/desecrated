400db0c5858f56f586718cf8863d5a46c3eb1a8e
Mitemi <miter360@gmail.com>
2026-06-21 13:37:00 -0400

    Proper blood unification

--- kubejs/client_scripts/itemNames.js
+++ kubejs/client_scripts/itemNames.js
@@ -3,14 +3,18 @@
 // Rename Item
 
 ClientEvents.lang('en_us', event => {
-  event.renameItem('ftbquests:book', 'Metaphysical Logbook');
-  event.renameItem('sons_of_sins:bloody_bone', 'Sinful Bone');
+  event.renameItem('ftbquests:book', 'Wise Grisha');
+
   event.renameItem('butchery:bottle_of_blood', 'Bottle of Raw Blood');
-  event.renameItem('sons_of_sins:heart', 'Fleshy Heart');
   event.renameItem('butchery:bone_skinning_knife', 'Bone Knife');
   event.renameItem('butchery:copper_skinning_knife', 'Copper Knife');
   event.renameItem('butchery:iron_skinning_knife', 'Iron Knife');
   event.renameItem('butchery:gold_skinning_knife', 'Gold Knife');
   event.renameItem('butchery:diamond_skinning_knife', 'Diamond Knife');
   event.renameItem('butchery:netherite_skinning_knife', 'Netherite Knife');
+
+  event.renameItem('sons_of_sins:bloody_bone', 'Sinful Bone');
+  event.renameItem('sons_of_sins:heart', 'Fleshy Heart');
+
+  event.renameItem('hexalia:small_cauldron', 'Brewing Cauldron');
 })
