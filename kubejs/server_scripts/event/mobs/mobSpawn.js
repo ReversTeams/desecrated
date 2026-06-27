871bda30174c4ed491c6d7fe29ab7e5ebaf2a45a
Mitemi <miter360@gmail.com>
2026-06-26 23:40:39 -0400

    Yore Part 1

--- kubejs/server_scripts/event/mobs/mobSpawn.js
+++ kubejs/server_scripts/event/mobs/mobSpawn.js
@@ -1,2 +1,7 @@
 //priority: 500
 // Mob Spawn Events
+EntityEvents.spawned(event => {
+  if (event.entity.type == "celestisynth:star_monolith") {
+    event.entity.potionEffects.add("minecraft:wither", 1250, 0, false, false) 
+  }
+})
