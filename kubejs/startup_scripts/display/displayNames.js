97eca1c848d23d7db4d76e6f7ff258c34244bb9a
Mitemi <miter360@gmail.com>
2026-07-08 23:47:04 -0400

    Artificer Part 3.7

--- kubejs/startup_scripts/display/displayNames.js
+++ kubejs/startup_scripts/display/displayNames.js
@@ -7,3 +7,5 @@ Platform.mods.oreberriesreplanted.name = 'Oreberries';
 Platform.mods.simpleenemymod.name = 'Victorian Hunters';
 Platform.mods.create.name = '???'; // CREATE IS NOT REAL CREATE IS NOT REAL CREATE IS NOT REAL CREATE IS NOT REAL
 Platform.mods.tetra.name = 'Tetra'; 
+Platform.mods.incision.name = 'Incision'; 
+Platform.mods.macabre.name = 'Macabre'; 
